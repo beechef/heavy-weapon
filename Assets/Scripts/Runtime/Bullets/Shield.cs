3998f28e731432d5369ea760f932d5e601b774c5
Chef <andelnguyen@gmail.com>
2022-07-10 09:47:35 +0700

    Adding Shield

---
+++ Assets/Scripts/Runtime/Bullets/Shield.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Runtime.Bullets
+{
+    public class Shield : BasicBullet
+    {
+        [SerializeField] private new SpriteRenderer renderer;
+        [SerializeField] private float scale = 1f;
+        [SerializeField] private bool isPlayOnAwake = true;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            if (isPlayOnAwake)
+            {
+                TurnOn();
+            }
+
+            Scale(scale);
+        }
+
+        public void Scale(float scale)
+        {
+            Transform cachedTransform = transform;
+            Vector3 localScale = cachedTransform.localScale;
+            localScale *= scale;
+            cachedTransform.localScale = localScale;
+        }
+
+        public void TurnOn()
+        {
+            renderer.enabled = true;
+            coll.enabled = true;
+        }
+
+        public void TurnOff()
+        {
+            renderer.enabled = false;
+            coll.enabled = false;
+        }
+
+        private void OnDisable()
+        {
+            TurnOff();
+        }
+    }
+}
