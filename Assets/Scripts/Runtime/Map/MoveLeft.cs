9fedef640c3c3343439b36eabc4c354afaa1b7c7
tqut214 <78657896+tqut214@users.noreply.github.com>
2022-07-22 09:00:30 +0700

    check in GamePlayState

---
+++ Assets/Scripts/Runtime/Map/MoveLeft.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float moveSpeed;
+    private SpriteRenderer sprite;
+    
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+       
+        if (transform.position.x < -(5.3+sprite.bounds.size.x/2))
+        {
+            Destroy(gameObject);
+        } 
+        transform.position += new Vector3(-moveSpeed * Time.deltaTime, 0);
+    }
+    
+}
