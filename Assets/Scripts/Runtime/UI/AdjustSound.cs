6641aadf0859876ec92af6ac51e99b26ae45eb88
Chef <andelnguyen@gmail.com>
2022-07-23 16:35:17 +0700

    Add Sound Settings, Add Custom Cursor When Hover UI

---
+++ Assets/Scripts/Runtime/UI/AdjustSound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Runtime.UI
+{
+    public class AdjustSound : MonoBehaviour
+    {
+        [SerializeField] private FloatVariable floatVariable;
+        [SerializeField] private Slider slider;
+
+        private void Awake()
+        {
+            slider.value = floatVariable.Value;
+        }
+
+        public void ChangeValue(float value)
+        {
+            floatVariable.Value = value;
+        }
+    }
+}
--- Assets/Scripts/Runtime/UI/MainMenuManager.cs
+++ Assets/Scripts/Runtime/UI/MainMenuManager.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
     private void OnEnable()
     {
-       
+                                                                                                              
     }
 }
